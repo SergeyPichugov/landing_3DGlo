72d5dde44bf54081264a5b95828e8e14bcbfeae1
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-23 17:00:13 +0300

    feat(script.js):feat

--- script/sctipt.js
+++ script/sctipt.js
@@ -43,7 +43,67 @@
 		let timerId = setInterval(updateClock, 1000);
 	}
 
-	countTimer('23 Feb 2021 7:00:31');
+	countTimer('28 Feb 2021 7:00:31');
+
+
+	//меню
+	const toogleMenu = ()=>{
+		const btnMenu =document.querySelector('.menu'),
+				menu = document.querySelector('menu'),
+				closeBtn = document.querySelector('.close-btn'),
+				menuItems = menu.querySelectorAll('ul>li');
+		
+		const handlerMenu = () => {
+			menu.classList.toggle('active-menu');
+		};
+
+		btnMenu.addEventListener('click', handlerMenu);
+		closeBtn.addEventListener('click', handlerMenu);
+		menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+	};
+
+	toogleMenu();
+
+	//popup
+	const togglePopUp = () => {
+		const popup = document.querySelector('.popup'),
+			popupBtn = document.querySelectorAll('.popup-btn'),
+			popUpClose = document.querySelector('.popup-close'),
+			widthWin = document.documentElement.clientWidth;
+
+		let count = 0;
+		let moveModal;
+
+		const animationModal = () => {
+			if (widthWin >= 768){
+				moveModal = requestAnimationFrame(animationModal);
+				count += 4;
+				if (count < 104){
+					popup.style.transform = `translateY(-${100 -count}%)`;
+				} else {
+					count = 0;
+					cancelAnimationFrame(moveModal);
+				}
+			}
+		};
+		
+		popupBtn.forEach((elem) => {
+			elem.addEventListener('click', () => {
+				popup.style.display = 'block';
+				moveModal = requestAnimationFrame(animationModal);
+			});
+		});
+
+		popUpClose.addEventListener('click', ()=> {
+			popup.style.display = 'none';
+		}); 
+	};
+
+	togglePopUp();
+
+
+
+
 
 
 });
