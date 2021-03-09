d3b33981cf786a244454ca54e1249da3508329eb
Sergey Pichugov <polarizaciacd@gmail.com>
2021-03-09 20:05:59 +0300

    feat(script.js): feat

--- script/script.js
+++ script/script.js
@@ -455,7 +455,7 @@
 
 	//send-ajax-form
 	const sendForm = () => {
-		const errorMassage = 'Что то пошлоне так',
+		const errorMassage = 'Что то пошло не так',
 				successMassage = 'Спасибо! Мы скоро с вами свяжемся!';
 		
 		const clearInput = (formClear) => {
@@ -483,14 +483,12 @@
 			statusMessage.appendChild(statusImg);
 
 			const formData = new FormData(target);
-			let body = {};
 
-			formData.forEach((val, key) => {
-				body[key] = val;
-			});
-
-			postData(body)
-				.then(() => {
+			postData(formData)
+				.then((responce) => {
+					if (responce.status !== 200){
+						throw new Error('status network not 200');
+					}
 					statusMessage.textContent = successMassage;
 					setTimeout(() => statusMessage.textContent = '', 4000);
 				})
@@ -504,25 +502,15 @@
 
 
 		const postData = (body) => {
-			return new Promise((resolve, reject) => {
-				const request = new XMLHttpRequest();
-
-				request.addEventListener('readystatechange', () => {
-					if (request.readyState !== 4) {
-						return;
-					}
-					if (request.status === 200) {
-						resolve();
-					} else {
-						reject(request.status);
-					}
-				});
-
-				request.open('POST', './server.php');
-				request.setRequestHeader('Contenr-Type', 'application/json');
-				request.send(JSON.stringify(body));
+			return fetch('./server.php', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'multipart/from-data'
+				},
+				body: JSON.stringify(body)
 
 			});
+
 			
 		};
 	};
